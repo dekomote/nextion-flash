f7dd9280713c0b3b67802608d2bbcd6cd3f7120c
Dejan Noveski <dr.mote@gmail.com>
2023-09-25 08:43:38 +0200

    Connection

--- nextion_flash/src/main.rs
+++ nextion_flash/src/main.rs
@@ -1,11 +1,97 @@
-use std::rc::Rc;
+use std::{rc::Rc, time::Duration, io::{Error, Read}, thread, cell::RefCell, borrow::BorrowMut};
 use clap::Parser;
+use serial2::SerialPort;
 
 mod logging;
 
-const COMMAND_START: [u8; 4] = [0xff, 0xff, 0xff, 0xff];
+const COMMAND_START: [u8; 4] = [0x00, 0xff, 0xff, 0xff];
 const COMMAND_STOP: [u8; 3] = [0xff, 0xff, 0xff];
-const COMMAND_CONNECT: [u8; 7] = [0x63, 0x6F, 0x6E, 0x6E, 0x65, 0x63, 0x74];
+const COMMAND_CONNECT: [u8; 14] = [0x00, 0xff, 0xff, 0xff, 0x63, 0x6f, 0x6e, 0x6e, 0x65, 0x63, 0x74, 0xff, 0xff, 0xff];
+
+
+pub struct Connection<'a>{
+    pub port: Rc<RefCell<SerialPort>>,
+    pub baud_rate: u32,
+    pub device: & 'a str,
+}
+
+impl<'a> Connection<'a> {
+    pub fn try_bauds(device: & 'a str) ->  Result<Connection, Error> {
+        let mut baud_rates = Vec::from(serial2::COMMON_BAUD_RATES);
+        baud_rates.sort_unstable_by(|a, b| b.partial_cmp(a).unwrap());
+
+        for (pos, baud_rate) in baud_rates.iter().enumerate() {
+            log::info!("Trying baud {baud_rate}");
+            match Connection::connect(device, *baud_rate) {
+                Ok(conn) => {
+                    log::info!("Connected at baud {baud_rate}");
+                    return Ok(conn);
+                }
+                Err(err) => {
+                    if pos == baud_rates.len() {
+                        return Err(err);
+                    }
+                    else {
+                        continue;
+                    }
+                }
+            }
+        }
+        Err(Error::new(std::io::ErrorKind::NotConnected, "Error trying bauds."))
+    }
+    pub fn new(device: & 'a str, baud_rate: u32) ->  Result<Connection, Error> {
+        Connection::connect(device, baud_rate)
+    }
+
+    fn get_connect_timeout(baud_rate: u32) -> Duration {
+        Duration::from_millis((30 + 11000000 / baud_rate).into())
+    }
+
+    fn purge_read(port: &mut SerialPort) {
+        loop {
+            let b: &mut [u8; 1] = &mut [0];
+            match port.read_exact(b) {
+                Ok(_) => {}
+                Err(_) => break
+            };
+        }
+    }
+    
+    fn connect(device: & 'a str, baud_rate: u32) -> Result<Connection, Error> {
+        let mut port = match serial2::SerialPort::open(device, baud_rate) {
+            Ok(port) => port,
+            Err(err) => {
+                return Err(err);
+            }
+        };
+
+        Connection::purge_read(&mut port);
+
+        match port.write_all(&COMMAND_CONNECT) {
+            Ok(_) => {
+                let buf: &mut [u8; 5] = &mut [0; 5];
+                
+                port.set_read_timeout(Connection::get_connect_timeout(baud_rate)).unwrap();
+                port.read_exact(buf).and_then(|()| {
+                    if buf == b"comok" {
+                        return Ok(
+                            Connection { port: Rc::new(RefCell::new(port)), baud_rate: baud_rate, device: device }
+                        );
+                    }
+                    else {
+                        return Err(
+                            Error::new(std::io::ErrorKind::NotConnected, "Connection not established. Try and restart the device")
+                        );
+                    }
+                })
+            },
+            Err(err) => {
+                Err(err)
+            }
+        }
+    }
+
+}
 
 
 #[derive(Parser, Debug)]
@@ -17,16 +103,16 @@ struct Args {
     /// On Windows, use something like "COM1".
     /// For COM ports above COM9, you need to use the win32 device namespace,
     /// for example "\\.\COM10" (or "\\\\.\\COM10" with string escaping).
-    #[arg()]
+    #[arg(default_value_t = String::from("/dev/ttyUSB1"))]
     serial_port: String,
 
     /// Path to the file to be flashed
-    #[arg()]
+    #[arg(default_value_t = String::from("1.txt"))]
     file_path: String,
 
     /// Initial baud rate.
     /// Faster baud rates will be tried if the setting is set.
-    #[arg(short, long, default_value_t = 9600)]
+    #[arg(short, long, default_value_t = 115200)]
     baud_rate: u32,
 
     /// Do not try faster baud negotiations.
@@ -40,16 +126,7 @@ fn main() {
 
     logging::init_logger();
 
-    let port = match serial2::SerialPort::open(args.serial_port, args.baud_rate) {
-        Ok(port) => port,
-        Err(err) => {
-            println!("Error opening serial port {}", err);
-            return;
-        }
-    };
-
-    let port = Rc::new(port);
-
-    // Lets negotiate
+    let connection = Connection::try_bauds(&args.serial_port).unwrap();
+    let connection = Connection::try_bauds(&args.serial_port).unwrap();
     
 }
